9fc2ec7e18765e0ca76fe0e5a5a7bdf8332f4f46
Mark Wang <zhengkaw@gmail.com>
2015-01-26 11:52:31 -0800

    find probability to draw at least x lands by turn y

--- LandCounter/AppDelegate.m
+++ LandCounter/AppDelegate.m
@@ -11,6 +11,10 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (nonatomic) NSInteger sizeOfDeckInt;
+@property (nonatomic) NSInteger numberOfLandsInt;
+@property (nonatomic) NSInteger wantedLandsInt;
+@property (nonatomic) NSInteger byTurnInt;
 @end
 
 @implementation AppDelegate
@@ -24,13 +28,53 @@
 }
 
 - (IBAction)calculatePercentage:(id)sender {
-    NSInteger sizeOfDeck = [_sizeOfDeck integerValue];
-    NSInteger numberOfLands = [_numberOfLands integerValue];
-    NSInteger wantedLands = [_wantedLands integerValue];
-    NSInteger byTurn = [_byTurn integerValue];
+    _sizeOfDeckInt = [_sizeOfDeck integerValue];
+    _numberOfLandsInt = [_numberOfLands integerValue];
+    _wantedLandsInt = [_wantedLands integerValue];
+    _byTurnInt = [_byTurn integerValue];
     
+    // Assuming on play for now. Is 6 because turn 1, you have 7 cards.
+    NSInteger numberOfCardsInHand = _byTurnInt + 6;
     
+    float probability = [self findProbabilityOfHavingAtLeastXLandsInYCards:_wantedLandsInt numberOfCardsInHard:numberOfCardsInHand];
+    NSLog(@"probability is %f", probability);
+}
+
+- (float)findProbabilityOfHavingAtLeastXLandsInYCards:(NSInteger)lands numberOfCardsInHard:(NSInteger)cards {
+    float probability = 0;
+    // Take sum of probability of lands from wanted lands to number of cards in hand.
+    for (NSInteger i = lands; i <= cards; i++) {
+        // Find probability of having i lands in numberOfCardsInHand cards.
+        probability = probability + [self findProbabilityOfHavingXLandsInYCards:i numberOfCardsInHand:cards];
+    }
     
+    return probability;
+}
+
+- (float)findProbabilityOfHavingXLandsInYCards:(NSInteger)lands numberOfCardsInHand:(NSInteger)cards {
+    // is (number of lands in deck choose number of lands) * (number of non lands in deck choose number of non lands in hand) / (deck size choose hand size).
+    
+    // (number of lands in deck choose number of lands)
+    NSInteger numberOfNonLandsInHand = cards - lands;
+    NSInteger numberOfNonLandsInDeck = _sizeOfDeckInt - _numberOfLandsInt;
+    float a = 1;
+    for (int i = 0; i < lands; i++) {
+        a = a * (_numberOfLandsInt - i) / (lands - i);
+    }
+    
+    // number of non lands in deck choose number of non lands in hand
+    float b = 1;
+    for (int i = 0; i < numberOfNonLandsInHand; i++) {
+        b = b * (numberOfNonLandsInDeck - i) / (numberOfNonLandsInHand - i);
+    }
+    
+    // deck size choose hand size
+    float c = 1;
+    for (int i = 0; i < cards; i++) {
+        c = c * (_sizeOfDeckInt - i) / (cards - i);
+    }
+    
+    return (a * b) / c;
 }
 
 @end
