569b0777fb4d61dac3233a84f8b61ad67a287140
Mark Wang <zhengkaw@gmail.com>
2015-01-26 20:19:02 -0800

    very hacky solution for two colored casting costs with dual lands

--- LandCounter/AppDelegate.m
+++ LandCounter/AppDelegate.m
@@ -21,13 +21,13 @@
 @property (nonatomic) NSInteger coloredSpellCostInt;
 @property (nonatomic) NSInteger playOrDrawNumberOfCards;
 
-@property (nonatomic) NSInteger *colorlessCostOfGoldSpell;
-@property (nonatomic) NSInteger *coloredCostOfGoldSpellA;
-@property (nonatomic) NSInteger *coloredCostOfGoldSpellB;
-@property (nonatomic) NSInteger *numberOfDualLands;
-@property (nonatomic) NSInteger *numberOfColoredLandsA;
-@property (nonatomic) NSInteger *numberOfColoredLandsB;
-@property (nonatomic) NSInteger *castGoldByTurn;
+@property (nonatomic) NSInteger colorlessCostOfGoldSpell;
+@property (nonatomic) NSInteger coloredCostOfGoldSpellA;
+@property (nonatomic) NSInteger coloredCostOfGoldSpellB;
+@property (nonatomic) NSInteger numberOfDualLands;
+@property (nonatomic) NSInteger numberOfColoredLandsA;
+@property (nonatomic) NSInteger numberOfColoredLandsB;
+@property (nonatomic) NSInteger castGoldByTurn;
 
 @end
 
@@ -60,7 +60,7 @@
 
 - (IBAction)calculateGoldCastPercentage:(id)sender {
     [self setNumbers];
-    float probability = [self runSimulations:@"cast" numberOfSimulation:50000];
+    float probability = [self runSimulations:@"gold" numberOfSimulation:50000];
     NSLog(@"probability is %f", probability);
     
 }
@@ -94,13 +94,20 @@
             if ([self getAtLeastXLandsInYCards:_wantedLandsInt numberOfCardsInHard:numberOfCardsInHand])
                 numberOfHits ++;
         }
-    } else {
+    } else if ([simulationType isEqualToString:@"cast"]) {
         // Different number of cards in hand, since two different inputs. 
         NSInteger numberOfCardsInHand = _castByTunInt + _playOrDrawNumberOfCards;
         for (int i = 0; i < numberOfSimulations; i++) {
             if ([self getAtLeastXColoredAndYColorlessInZCards:_coloredSpellCostInt colorless:_colorlessSpellCostInt cards:numberOfCardsInHand])
                 numberOfHits ++;
         }
+    } else {
+        NSInteger numberOfCardsInHand = _castGoldByTurn + _playOrDrawNumberOfCards;
+        for (int i = 0; i < numberOfSimulations; i++) {
+            if ([self canCastGoldSpellInXCards:numberOfCardsInHand])
+                numberOfHits ++;
+        }
+
     }
     
     return numberOfHits / (float) numberOfSimulations ;
@@ -168,6 +175,72 @@
         return YES;
     else
         return NO;
+}
+
+- (BOOL)canCastGoldSpellInXCards:(NSInteger)cards {
+    // Bah, I'll generalize it tomorrow.
+    
+    NSMutableIndexSet *randomNumbersAlreadyGenerated = [[NSMutableIndexSet alloc] init];
+    int numberOfLandsInHand = 0;
+    int numberOfDualLandsInHand = 0;
+    int numberOfColoredLandsAInHand = 0;
+    int numberOfColoredLandsBInHand = 0;
+    
+    NSInteger numberOfRelevantCardsInHand = cards - 1;
+    // This is sizeOfDeck - 1 because one again, assumed that one of the cards is the spell you want to cast.
+    NSInteger numberOfRelevantCardsInDeck = _sizeOfDeckInt - 1;
+    
+    for (int i = 0; i < numberOfRelevantCardsInHand; i++) {
+        // First, make an NSSet that contains the numbers already generated.
+        int r;
+        // Generate a random number until you get one that hasn't been hit yet.
+        do {
+            r = arc4random_uniform(numberOfRelevantCardsInDeck);
+        } while ([randomNumbersAlreadyGenerated containsIndex:r]);
+        // So this r is not in the index set yet, so add it.
+        [randomNumbersAlreadyGenerated addIndex:r];
+        
+        // Want less than since r starts at 0 and ends at _sizeOfDeckInt - 2.
+        if (r < _numberOfDualLands) {
+            numberOfDualLandsInHand++;
+            numberOfLandsInHand++;
+        } else if (r < (_numberOfDualLands + _numberOfColoredLandsA)) {
+            numberOfColoredLandsAInHand++;
+            numberOfLandsInHand++;
+        } else if (r < (_numberOfDualLands + _numberOfColoredLandsA + _numberOfColoredLandsB)) {
+            numberOfColoredLandsBInHand++;
+            numberOfLandsInHand++;
+        } else if (r < _numberOfLandsInt) {
+            numberOfLandsInHand++;
+        }
+    }
+    
+    NSInteger totalCost = _colorlessCostOfGoldSpell + _coloredCostOfGoldSpellA + _coloredCostOfGoldSpellB;
+    if (numberOfLandsInHand < totalCost)
+        return NO;
+    
+    // Don't have to check for number of lands in hand since the above already checks to make that true.
+    // If can cast without dual land, you're fine.
+    if (_coloredCostOfGoldSpellA <= numberOfColoredLandsAInHand &&  _coloredCostOfGoldSpellB <= numberOfColoredLandsBInHand )
+        return YES;
+    
+    // Now checks to see about dual land. Take sum of the lack of mana for the two colors, then sees if the dual land can cover the sum of difference.
+    int coloredLandDifference = 0;
+    if (numberOfColoredLandsAInHand < _coloredCostOfGoldSpellA) {
+        coloredLandDifference = coloredLandDifference + (_coloredCostOfGoldSpellA - numberOfColoredLandsAInHand);
+    }
+    
+    if (numberOfColoredLandsBInHand < _coloredCostOfGoldSpellB) {
+        coloredLandDifference = coloredLandDifference + (_coloredCostOfGoldSpellB - numberOfColoredLandsBInHand);
+    }
+    
+    if (coloredLandDifference <= numberOfDualLandsInHand)
+        return YES;
+    
+    // defaults to return no. 
+    return NO;
+
+    
 }
 
 @end
